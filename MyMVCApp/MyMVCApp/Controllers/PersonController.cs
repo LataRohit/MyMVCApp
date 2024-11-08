e51aac20acfac7469ba3e4e55bb60392d17b88fa
LataRohit <96121619+LataRohit@users.noreply.github.com>
2024-11-08 20:14:13 +0530

    Added project files

---
+++ MyMVCApp/MyMVCApp/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using FormValidationGridApp.Models;
+
+namespace FormValidationGridApp.Controllers
+{
+    public class PersonController : Controller
+    {
+        // In-memory list to store persons (in real applications, use a database)
+        private static List<Person> personList = new List<Person>();
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(personList); // Pass the list to the grid view
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                personList.Add(person); // Add the person to the list
+                return RedirectToAction("Index"); // Redirect to show grid view
+            }
+            return View(person); // Show form with validation errors if invalid
+        }
+    }
+}
